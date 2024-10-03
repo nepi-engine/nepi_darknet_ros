6281293964ab1c7e5126cd364b3cd7cbc99fe85f
Jason Seawall <jseawall@numurus.com>
2024-10-03 08:04:47 -0700

    updates for ai framework support files

--- darknet_ros/if/darknet_if.py
+++ darknet_ros/if/darknet_if.py
@@ -14,6 +14,7 @@ import numpy as np
 
 from nepi_edge_sdk_base import nepi_ros
 
+
 from std_msgs.msg import Empty, Float32
 from nepi_ros_interfaces.msg import ObjectCount
 from nepi_ros_interfaces.srv import ImageClassifierStatusQuery, ImageClassifierStatusQueryResponse
@@ -36,34 +37,38 @@ AI_DICT = dict(
 )
 
 class DarknetAIF(object):
-    darknet_set_threshold_pub = None
-    classifier_state = ImageClassifierStatusQueryResponse.CLASSIFIER_STATE_STOPPED
-    def __init__(self, ai_dict):
-      
+    def __init__(self, ai_dict,node_namespace):
+      if node_namespace[-1] == "/":
+        node_namespace = node_namespace[:-1]
+      self.node_namespace = node_namespace
       self.pkg_name = ai_dict['pkg_name']
       self.node_name = ai_dict['node_name']
       self.launch_file = ai_dict['launch_file']
       self.model_prefix = ai_dict['model_prefix']
       self.model_search_path = ai_dict['models_path']
+      threshold_namespace = self.node_namespace + '/nepi_darknet_ros/set_threshold'
+      self.set_threshold_pub = rospy.Publisher(threshold_namespace, Float32, queue_size=1, latch=True)
     
     #################
     # Darknet Model Functions
 
     def getModelsDict(self):
+        models_dict = dict()
         classifier_name_list = []
         classifier_size_list = []
         classifier_classes_list = []
         # Try to obtain the path to Darknet models from the system_mgr
-        darknet_cfg_path_config_folder = os.path.join(self.model_search_path, 'config')
+        cfg_path_config_folder = os.path.join(self.model_search_path, 'config')
+        rospy.loginfo("Darknet looking for models config files in folder: " + cfg_path_config_folder)
         # Grab the list of all existing darknet cfg files
-        self.darknet_cfg_files = glob.glob(os.path.join(darknet_cfg_path_config_folder,'*.yaml'))
+        self.cfg_files = glob.glob(os.path.join(cfg_path_config_folder,'*.yaml'))
         # Remove the ros.yaml file -- that one doesn't represent a selectable trained neural net
         try:
-            self.darknet_cfg_files.remove(os.path.join(darknet_cfg_path_config_folder,'ros.yaml'))
+            self.cfg_files.remove(os.path.join(cfg_path_config_folder,'ros.yaml'))
         except:
-            rospy.logwarn("Unexpected: ros.yaml is missing from the darknet config path " + darknet_cfg_path_config_folder)
+            rospy.logwarn("Unexpected: ros.yaml is missing from the darknet config path " + cfg_path_config_folder)
 
-        for f in self.darknet_cfg_files:
+        for f in self.cfg_files:
             yaml_stream = open(f, 'r')
             # Validate that it is a proper config file and gather weights file size info for load-time estimates
             cfg_dict = yaml.load(yaml_stream)
@@ -87,7 +92,6 @@ class DarknetAIF(object):
             #rospy.logwarn("classes: " + str(classifier_classes_list))
             classifier_name_list.append(classifier_name)
             classifier_size_list.append(os.path.getsize(weight_file))
-        models_dict = dict()
         for i,name in enumerate(classifier_name_list):
             model_name = self.model_prefix + name
             model_dict = dict()
@@ -95,7 +99,7 @@ class DarknetAIF(object):
             model_dict['size'] = classifier_size_list[i]
             model_dict['classes'] = classifier_classes_list[i]
             models_dict[model_name] = model_dict
-            #rospy.logwarn("Classifier returning models dict" + str(models_dict))
+        #rospy.logwarn("Classifier returning models dict" + str(models_dict))
         return models_dict
 
 
@@ -103,7 +107,7 @@ class DarknetAIF(object):
         # Build Darknet new classifier launch command
         launch_cmd_line = [
             "roslaunch", self.pkg_name, self.launch_file,
-            "namespace:=" + nepi_ros.get_node_namespace(), 
+            "namespace:=" + self.node_namespace, 
             "yolo_weights_path:=" + os.path.join(self.model_search_path, "yolo_network_config/weights"),
             "yolo_config_path:=" + os.path.join(self.model_search_path, "yolo_network_config/cfg"),
             "ros_param_file:=" + os.path.join(self.model_search_path, "config/ros.yaml"),
@@ -112,41 +116,27 @@ class DarknetAIF(object):
             "detection_threshold:=" + str(threshold)
         ]
         rospy.loginfo("Launching Darknet ROS Process: " + str(launch_cmd_line))
-        self.darknet_ros_process = subprocess.Popen(launch_cmd_line)
-        self.darknet_set_threshold_pub = rospy.Publisher('nepi_darknet_ros/set_threshold', Float32, queue_size=1, latch=True) # Must match the node that gets launched by darknetStartClassifier()
+        self.ros_process = subprocess.Popen(launch_cmd_line)
+        
 
         # Setup Classifier Setup Tracking Progress
-        self.classifier_state = ImageClassifierStatusQueryResponse.CLASSIFIER_STATE_LOADING        
-        self.darknet_update_sub = rospy.Subscriber('ai_detector_mgr/found_object', ObjectCount, self.darknetUpdateCb) # Resubscribe to found_object so that we know when the classifier is up and running again
-            
-    def darknetUpdateCb(self, msg):
-        # Means that darknet is up and running
-        self.classifier_state = ImageClassifierStatusQueryResponse.CLASSIFIER_STATE_RUNNING
-        if not (None == self.darknet_update_sub):
-            self.darknet_update_sub.unregister()
+
+
+
 
     def stopClassifier(self):
         rospy.loginfo("Stopping classifier")
-        if not (None == self.darknet_update_sub):
-            self.darknet_update_sub.unregister()
-        if not (None == self.darknet_found_object_sub):
-            self.darknet_found_object_sub.unregister()
-            self.darknet_found_object_sub = None
-        if not (None == self.darknet_bounding_boxes_sub):
-            self.darknet_bounding_boxes_sub.unregister()
-            self.darknet_bounding_boxes_sub = None
-        if not (None == self.darknet_ros_process):
-            self.darknet_ros_process.terminate()
-            self.darknet_ros_process = None
+        if not (None == self.ros_process):
+            self.ros_process.terminate()
+            self.ros_process = None
         self.current_classifier = "None"
         self.current_img_topic = "None"
+        
         #self.current_threshold = 0.3
 
     def updateClassifierThreshold(self,threshold):
-        darknet_set_threshold_pub.publish(theshold)
+        set_threshold_pub.publish(theshold)
 
-    def getClassifierState(self):
-      return self.classifier_state
 
     
 
